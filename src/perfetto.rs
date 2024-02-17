1252e2393c9a8a2a17059184d80c76328bfcfa2d
Yusuke Tsutsumi <yusuke.tsutsumi@getcruise.com>
2024-02-16 22:34:47 -0800

    feat: adding benchmarks for proto serialization
    results from bench tests:
    
    	test perfetto::tests::bench_create_example_trace ... bench:   2,149,230 ns/iter (+/- 722,057)
    	test tests::bench_add_example_profile            ... bench:     936,466 ns/iter (+/- 179,738)
    
    without deserialization, protos take ~ 1/2 of the time.
    without serialization or deserialization, protos take ~/10 of the time.
    
    So it's clear that flatbuffers work best when it's expected for both a
    serialization and a deserialization to occur in at the same time. Or,
    incurring a slight overhead on serialization for a much lower overhead
    on deserialization later.

--- src/perfetto.rs
+++ src/perfetto.rs
@@ -1,7 +1,48 @@
 use super::schema_generated;
 use perfetto_trace::perfetto::protos;
+use prost::Message;
 
 // generate_example_trace returns back a protos::Trace object.
 pub fn to_prefetto_trace(profile: schema_generated::Profile) -> protos::Trace {
     return protos::Trace { packet: vec![] };
+}
+
+// create an example perfetto trace
+pub fn create_example_trace() -> protos::Trace {
+    let mut packets = vec![];
+    for i in 0..1000 {
+        let packet = protos::TracePacket {
+            timestamp: Some(0),
+            timestamp_clock_id: None,
+            trusted_pid: None,
+            interned_data: None,
+            sequence_flags: None,
+            incremental_state_cleared: None,
+            trace_packet_defaults: None,
+            previous_packet_dropped: Some(false),
+            first_packet_on_sequence: None,
+            machine_id: None,
+            data: None,
+            optional_trusted_uid: None,
+            optional_trusted_packet_sequence_id: None,
+        };
+        packets.push(packet);
+    }
+    let mut trace = protos::Trace { packet: packets };
+    return trace;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[bench]
+    fn bench_create_example_trace(b: &mut test::Bencher) {
+        b.iter(|| {
+            let trace = create_example_trace();
+            let mut buf = vec![];
+            trace.encode(&mut buf).unwrap();
+            let trace2 = protos::Trace::decode(&*buf).unwrap();
+        });
+    }
 }
